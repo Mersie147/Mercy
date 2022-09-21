89a1682466b2c086fd4b98ffe13dc46d40aa46e1
Wanyoike Mercy <114069502+Mersie147@users.noreply.github.com>
2022-09-21 14:35:16 +0300

    Add files via upload

---
+++ mercy.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract tech4dev{
+  function places()public return(string memory){
+    return "how are you";
+  
+}
+
+}
+contract Damilare is tech4dev{
+function user() public return(string memory) {
+  return "this is the second contract";
+}
+
+}
+
